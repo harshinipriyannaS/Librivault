188329fa09b314f33e9feb5f9fa83b34c5081f93
harshinipriyannaS <harshinipriyannaselvam@gmail.com>
2025-10-30 14:31:01 +0530

    Changes done for localhost

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:8080/api',
+  apiUrl: 'http://18.218.51.154:8080/api',
   appName: 'LibriVault',
   version: '1.0.0'
 };
